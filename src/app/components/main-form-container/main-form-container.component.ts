aad0d48cedaec2f899fdf7afccc82dce7eb666cf
ZeroZeroOne-dev <254635+ZeroZeroOne-dev@users.noreply.github.com>
2023-12-01 00:14:00 +0100

    mfc

---
+++ src/app/components/main-form-container/main-form-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-form-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-form-container.component.html'
+})
+export class MainFormContainerComponent {
+
+}
